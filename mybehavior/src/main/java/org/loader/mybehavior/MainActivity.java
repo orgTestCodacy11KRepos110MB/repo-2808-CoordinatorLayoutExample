73eae45cd6de7f94f001cf526f59a94e0e36d7b8
xujun <973243764@qq.com>
2016-12-01 15:59:37 +0800

    增加模仿知乎，模仿简书的效果

--- mybehavior/src/main/java/org/loader/mybehavior/MainActivity.java
+++ mybehavior/src/main/java/org/loader/mybehavior/MainActivity.java
@@ -1,21 +1,24 @@
 package org.loader.mybehavior;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_2);
+        setContentView(R.layout.activity_main);
 
-//        final TextView depentent = (TextView) findViewById(R.id.depentent);
-//        depentent.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                ViewCompat.offsetTopAndBottom(v, 5);
-//            }
-//        });
+        final TextView depentent = (TextView) findViewById(R.id.depentent);
+        depentent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewCompat.offsetTopAndBottom(v, 5);
+            }
+        });
     }
 }
